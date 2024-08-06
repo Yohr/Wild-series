3b6f8b8e7791d5cdb36825a05be50fc46b3a30f3
yohann renouard <yohann.renouard@gmail.com>
2024-08-06 11:54:41 +0200

    added programAction

--- server/app/controllers/categoryAction.js
+++ server/app/controllers/categoryAction.js
@@ -1,44 +1,39 @@
+const tables = require("../../database/tables");
+
 // Some data to make the trick
 
 const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
-  
-  // Declare the actions
-  
-const browse = (req, res) => {
-    if(req.query.q !=null){
-        const filteredCategories = categories.filter((categorie) => 
-            categorie.name.includes(req.query.q)
-    );
-    res.json(filteredCategories);
-    } else {
-        res.json(categories)
-    }
+	{
+		id: 1,
+		name: "Science-Fiction",
+	},
+	{
+		id: 2,
+		name: "Comédie",
+	},
+];
+
+// Declare the actions
+
+const browse = async (req, res) => {
+	const categoriesFromDB = await tables.category.readAll();
+
+	res.json(categoriesFromDB);
 };
 
 const read = (req, res) => {
+	console.info(req.params);
+	const parsedId = Number.parseInt(req.params.id, 10);
 
-    console.info(req.params);
-    const parsedId = parseInt(req.params.id, 10);
+	const categorie = categories.find((p) => p.id === parsedId);
 
+	if (categorie != null) {
+		res.json(categorie);
+	} else {
+		res.sendStatus(404);
+	}
+};
 
-    const categorie = categories.find((p) => p.id === parsedId);
+// Export them to import them somewhere else
 
-    if(categorie != null){
-        res.json(categorie);
-    } else {
-        res.sendStatus(404)
-    }
-  };
-  
-  // Export them to import them somewhere else
-  
-  module.exports = { browse, read };
+module.exports = { browse, read };
